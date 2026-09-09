a9b381dd8e69525a61d85a4fdac49e85469d3d5e
Laziz Shakarov <shakarovlaziz243@gmail.com>
2026-09-09 20:28:30 +0500

    fix(eslint): set @typescript-eslint/no-explicit-any to warning

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,6 +14,7 @@ export default defineConfig([
       "react-hooks/purity": "off",
       // O'zbek tilidagi tutuq belgilari (o', g') va tirnoqlar uchun
       "react/no-unescaped-entities": "off",
+      "@typescript-eslint/no-explicit-any": "warn",
     },
   },
   {
